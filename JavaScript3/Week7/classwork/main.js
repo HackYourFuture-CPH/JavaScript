d3c28d91e610700d7383ef818cf884f49609a907
behuFTP <benjamin.dals.hughes@gmail.com>
2018-11-05 15:47:26 +0100

    Add comments to code written in class

--- JavaScript3/Week7/classwork/main.js
+++ JavaScript3/Week7/classwork/main.js
@@ -1,11 +1,12 @@
 console.log('Script loaded');
 
 const helloAdder = function(string) {
-    /* replacemnet*/
+    // The parameter string works like a placeholder for the argument you call the function with
     /*
     console.log(string);
     console.log('hello' + string);
     */
+    // If nothing is returned, the function returns undefined
     return 'hello' + string;
 };
 
@@ -15,20 +16,14 @@ console.log(badrHello);
 
 const helloAdderArrow = string => {
     console.log(string);
-    
+    // this is an expression
     'hello' + string
+    // No return
 };
-
-
 
 const badrHelloArrow = helloAdderArrow('badr');
 console.log(badrHelloArrow);
 
-
-fetch('https://reddit.com/r/ProgrammerHumor.json')
-    .then(function(response) {
-        console.log(response);
-    });
 
 
 fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
@@ -36,26 +31,33 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
     .then((movies) => {
         console.log(movies);
         
+        // these two variables are functions
+        const getNewerMovies = movie => movie.year > 2006;
+        const getTitles = movie => movie.title;
+        
+        const newerMovies = movies
+            .filter(movie => movie.year > 2006);
+        const newerMoviesTitles = newerMovies
+            .map(movie => movie.title);
+
+        // using chaining and function composition
+        const newerMoviesTitlesChaining = movies
+            .filter(getNewerMovies)
+            .map(getTitles);
+
+        console.log(newerMoviesTitles);
+        console.log(newerMoviesTitlesChaining);
+        
         const longerMovies = movies
             .filter((movie) => {
-                console.log(movie);
-                console.log(numberOfWords);
                 const numberOfWords = movie.title.split(' ').length;
-                
+                // this statement is a boolean: numberOfWords > 6
                 return numberOfWords > 6;
-
-
-            })
+            });
 
             const reducer = function(acc, movie) {
                 return acc + movie.rating;
-            } 
+            };
 
             totalRating = movies.reduce(reducer, 0);
-
-        
-        
-        
-        
-    })
-    
+    });
