b27480a3373f9bc64ad927d6c89bc192f21a584a
behuFTP <benjamin.dals.hughes@gmail.com>
2019-01-06 16:33:28 +0100

    Add week4 classwork

--- JavaScript2/Week4/classwork/main.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('div').style.backgroundColor = 'yellow';
-document.querySelector('div').innerHTML = 'asd';
-
-const div = document.createElement('div');
-div.innerHTML = 'asdasdsd';
-
-document.querySelector('body').appendChild(div);
-
-
