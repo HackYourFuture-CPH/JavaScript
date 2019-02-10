dc11859e0ce958349bebadb297a6072a2ca418cf
senner008 <nielshtg@gmail.com>
2019-02-10 18:06:38 +0100

    comments

--- JavaScript3/Week9/classwork/buggy-object/solution/transition.js
+++ JavaScript3/Week9/classwork/buggy-object/solution/transition.js
@@ -4,12 +4,12 @@ var pageSize = document.documentElement.clientWidth;
 var obj = {
     div : document.querySelector('div'),
     isMoving : false, // is currently moving
-    // EDIT : normal function
+    // EDIT : normal function instead of arrow function. Here the this object will point to obj.
     move : function () {
-        // EDIT : should use offsetWidth since style object oly contains inline css
+        // EDIT : should use offsetWidth since style object only contains inline css
         this.div.style.transform = "translateX(" + (pageSize - this.div.offsetWidth) + "px)";
         this.isMoving = true;
-        // EDIT : using arrow function as callback if we want the this in the function to point to obj
+        // EDIT : using arrow function as callback if we want the this in the function to point to obj. Otherwise this will point to this.div
         this.div.addEventListener('transitionend', () => {
             this.isMoving = false;
         })  
