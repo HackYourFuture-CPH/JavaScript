1e121344a8178c2c06dfeb0c7fdbb7ded2a71bf1
senner008 <nielshtg@gmail.com>
2019-02-10 17:16:52 +0100

    object literals properties and methods

---
+++ JavaScript3/Week9/classwork/obj-literals/props-methods.js
@@ -0,0 +1,33 @@
+// Properties and methods on an object;
+
+var a = 1,
+   b = 2,
+   c = 3,
+   g = "baz";
+
+var o = {
+   a,
+   b,
+   c,
+   d() {
+      return this.c;
+   },
+   get e() {
+      return this.a
+   },
+   set f(val) {
+      this.a = val;
+   },
+   [g] : "Foo"
+};
+
+delete o.b // delete prop
+
+o.f = 5; // setter prop
+
+var prop_d = o.d;
+console.log(prop_d()); // undefined - The this in the function now points to the global scope
+console.log(prop_d.call(o)) // 3 - call function in o context
+var boundD = prop_d.bind(o); // permanently bind o object as this
+console.log(boundD()) // 3
+
