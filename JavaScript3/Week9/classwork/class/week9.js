e7191b53cbd0378149f9cd52eb53689ec68cd481
behuFTP <benjamin.dals.hughes@gmail.com>
2019-02-10 16:35:06 +0100

    Added human example

--- JavaScript3/Week9/classwork/class/week9.js
+++ JavaScript3/Week9/classwork/class/week9.js
@@ -8,19 +8,29 @@ const user = {
     }
 }
 
+
+class Human {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+let adil = new Human('adil');
+console.log(adil);
+console.log(adil.getName());
+
+let benjamin = new Human('benjamin');
+benjamin.getName();
+
 class Comment {
     constructor(username, content, time) {
         this.username = username;
         this.content = content;
-        this.time = time;        
-    }
-
-    renderJson() {
-        return JSON.stringify({
-            username: this.username,
-            content: this.content,
-            time: this.time,
-        });
+        this.time = time;
     }
 }
 
@@ -64,6 +74,10 @@ class Post {
     save() {
 
     }
+
+    logThis() {
+        console.log(this.username);
+    }
 }
 
 const post1 = new Post('benna100', 'asd', '10/02/1019', [], [], []);
@@ -75,5 +89,7 @@ console.log(post1.likes);
 post1.addComment('ugg', 'Great post', '14:16');
 console.log(post1.comments);
 
+post1.logThis();
+post2.logThis();
 
 
