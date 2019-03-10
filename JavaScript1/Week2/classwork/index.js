3e4f2a1aecef3fd5e18db6a0e63249c67212d93b
Emil Bay <github@tixz.dk>
2019-03-10 12:53:57 +0100

    Add examples with assignment operators

---
+++ JavaScript1/Week2/classwork/index.js
@@ -0,0 +1,27 @@
+var n = 10
+
+// Assignment operators
+n = n + 1 // exactly the same as the next line
+n += 1
+n += 2
+
+n *= 2
+n /= 2
+
+n -= 20
+
+// Increment
+n = 1
+console.log(n)
+var oldValue = n++ // gives back the old value
+console.log(n)
+console.log(oldValue)
+var newValue = ++n // gives back the new value
+console.log(n)
+console.log(newValue)
+
+// can also do decrementing
+n-- // gives old value
+--n // gives new value
+
+console.log(n)
