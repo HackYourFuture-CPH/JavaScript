0fb61f72de8dd976667bf6efe223324bfee31777
behuFTP <benjamin.dals.hughes@gmail.com>
2019-03-25 16:47:52 +0100

    Update week 8

---
+++ JavaScript3/Week8/homework/promise-visual/move-box.js
@@ -0,0 +1,65 @@
+// Creating new scope
+{
+    const redBox = document.querySelector('ul.marks li:nth-child(1)');
+    const blueBox = document.querySelector('ul.marks li:nth-child(2)');
+    const greenBox = document.querySelector('ul.marks li:nth-child(3)');
+    const boxes = [redBox, blueBox, greenBox];
+
+    const span = document.querySelector('span');
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function randomizeStartPosition() {
+        boxes.forEach(box => {
+            const x = getRandomInt(-20, 20);
+            const y = getRandomInt(-20, 20);
+            
+            box.style.left = x;
+            box.style.top = y;
+        });
+    }
+
+    randomizeStartPosition();
+
+
+    const targets = document.querySelectorAll('ul.targets li');
+    // continously check if circles has been added to the right targets
+    setInterval(() => {
+        setTargetFulfilled(redBox, targets[0], {x: 20, y: 300});
+        setTargetFulfilled(blueBox, targets[1], {x: 400, y: 300});
+        setTargetFulfilled(greenBox, targets[2], {x: 400, y: 20});
+        
+        const allTargetsFulfilled = [...targets].every(target => target.classList.contains('fulfilled'));
+        if (allTargetsFulfilled) {
+            setTimeout(() => span.classList.add('shown'), 300);
+        }
+    }, 10);
+
+    function setTargetFulfilled(box, targetBox, fulfilledPosition) {
+        const renderedClientBox = box.getBoundingClientRect();
+        
+        if (renderedClientBox.left === fulfilledPosition.x && renderedClientBox.top === fulfilledPosition.y) {
+            targetBox.classList.add('fulfilled');
+        }
+    }
+
+
+    /**
+     * @param {DOMelement} boxToMove - A DOM element of the box to move
+     * @param {Position} newPosition - An object specifying the new position of the box. x, y
+     * @return {Promise<any>}
+     */
+
+    function moveElement(boxToMove, newPosition) {
+        return new Promise(resolve => {
+            boxToMove.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
+            boxToMove.addEventListener('transitionend', resolve);
+        });
+    }
+
+    window.moveElement = moveElement;
+}
--- JavaScript3/Week8/readme.md
+++ JavaScript3/Week8/readme.md
@@ -5,3 +5,35 @@
 # Relevant links
 * [Preparation](preparation.md)
 * [Homework](homework.md)
+
+## Promise chaining
+Just like we can chain array functions like this:
+
+```js
+const fastCarBrands = cars
+    .filter(car => car.speed > 60)
+    .map(car => car.brand);
+```
+
+We can chain promise function aswell, and it works exactly like with chaining array methods or chaining anything in js. We use the return of calling the previous function:
+
+```js
+doesBenjaminEndWithN = "BENJAMIN"
+    .toLowerCase() // <-- toLowerCase returns a string!
+    .endsWith('n'); // <-- That string we can call .endsWith on!
+```
+
+When we deal with promises the chaining part comes because calling the `.then` function [returns a promises](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then)!
+
+We know that when we have a promise, we can call `.then` on that promise
+
+```js
+fetch('http://api.open-notify.org/astros.json')
+    .then(function(response) { // <-- .then returns a promise! 
+        // The returned promise (from calling .then) resolves when response.json() resolves!
+        return response.json(); 
+    })
+    .then(function(astronautData) { // because the previous .then function returns a promise, we can call .then on that!
+
+    });
+```
