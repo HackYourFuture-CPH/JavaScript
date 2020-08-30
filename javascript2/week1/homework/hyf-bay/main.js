9121f769467eacb1c4dc2258d178c2c8f669ea69
benna100 <benna100@users.noreply.github.com>
2020-08-30 11:11:17 +0200

    Update main.js

--- javascript2/week1/homework/hyf-bay/main.js
+++ javascript2/week1/homework/hyf-bay/main.js
@@ -1,15 +1,4 @@
 console.log("Script loaded");
 
 const products = getAvailableProducts();
-
-function renderProducts(products) {
-  // your code here
-  const ul = document.querySelector("ul");
-  products.forEach(product => {
-    const li = document.createElement("li");
-    li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
-    ul.appendChild(li);
-  });
-}
-
-renderProducts(products);
+console.log(products);
